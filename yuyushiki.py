6e53f775673d8f827d624900c6da51ba12f556b4
non <mail@non117.com>
2014-08-04 22:46:15 +0900

    戻るボタン実装. リファクタリングしました. L('ω')┘└('ω')」('乂')

--- yuyushiki.py
+++ yuyushiki.py
@@ -31,29 +31,43 @@ def insert(path, script, characters, reedit, useless):
             'reedit':reedit, 'useless':useless}
     collection.insert(d)
 
+def upsert(path, script, characters, reedit, useless):
+    d = {'path':path, 'script':script, 'characters':characters,
+            'reedit':reedit, 'useless':useless}
+    collection.update({'path':path}, d, upsert=True) 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    prev = False
     if request.method == 'POST':
         data = request.form
-        script = data.get('script', '')
-        path = data.get('path')
-        characters = [] #TODO
-        reedit = bool(data.get('reedit', False))
-        useless = bool(data.get('useless', False))
-        if find_one(path):
-            print('commit was duplicated.')
+        if data.get('action') == 'prev': #前の画像に戻る
+            path = data.get('path')
+            prev = True
         else:
-            insert(path, script, characters, reedit, useless)
-    latest = get_latest()
-    if latest == []:
-        p = pages[0]
-    else:
-        for i, p in enumerate(pages):
-            if p.as_posix() == latest[0]['path']:
-                break
-        p = pages[i+1]
+            script = data.get('script', '')
+            path = data.get('path')
+            characters = [] #TODO
+            reedit = bool(data.get('reedit', False))
+            useless = bool(data.get('useless', False))
+            upsert(path, script, characters, reedit, useless)
+        
+        i = pages.index(Path(path))
+        if prev:
+            p = pages[i-1]
+        else:
+            p = pages[i+1]
+    elif request.method == 'GET':
+        latest = get_latest()
+        if latest == []:
+            p = pages[0]
+        else:
+            i = pages.index(Path(latest[0]['path']))
+            p = pages[i+1]
+    
+    prev_data = find_one(p.as_posix())
     progress = round(collection.count() * 100 / len(pages), 2)
-    return render_template('index.tpl.html', path=p, progress=progress)
+    return render_template('index.tpl.html', path=p, progress=progress, prev_data=prev_data)
 
 @app.route('/data/<path:filename>')
 def static_yuyushiki(filename):
