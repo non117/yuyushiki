346172f3e3b9668ad57668f27bd95daaf7d2213d
tyage <namatyage@gmail.com>
2014-08-04 15:42:02 +0900

    autofocus使うように変更

--- templates/main.js
+++ templates/main.js
@@ -1,9 +1,4 @@
 $(function(){
-	$(document).ready(function(){
-		setTimeout(function(){
-			$('#textbox').focus();
-		}, 1);
-	});
 	$('input').focus(function(){
 		$(this).parent().addClass('focus');
 	}).blur(function(){
