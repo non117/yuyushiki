1826cb47f27f3bc44f107ac99f569bcc750dea75
non <mail@non117.com>
2014-08-07 02:36:38 +0900

    一気に書きすぎてわけわからん (,,Ծ﹏Ծ,, )

--- templates/main.js
+++ templates/main.js
@@ -1,8 +1,20 @@
+function setVal(obj){
+	var id = $(obj).attr("id");
+	if($(obj).hasClass('selected')){
+		$(obj).removeClass('selected');
+		$("#"+id+"_input").val('');
+	}else{
+		$(obj).addClass('selected');
+		$("#"+id+"_input").val(id);
+	}
+};
+
 $(function(){
 	$('input').focus(function(){
 		$(this).parent().addClass('focus');
 	}).blur(function(){
 		$(this).parent().removeClass('focus');
 	});
+
 
 });
--- yuyushiki.py
+++ yuyushiki.py
@@ -21,19 +21,22 @@ pages = reduce(lambda a, b:a+b, [[p for p in path.iterdir()] for path in root.it
 pages.sort()
 
 def get_latest():
-    ret = list(collection.find({}).sort('_id',-1).limit(1))
-    try:
-        return ret[0]
-    except IndexError:
-        return []
+    latest = list(collection.find({}).sort('_id',-1).limit(1))
+    if latest == []:
+        return pages[0]
+    else:
+        i = pages.index(Path(latest[0]['path']))
+        return pages[i+1] 
 
 def get_tag_latest():
-    # タグがついてないレコードの最も古いやつを返す
-    ret = list(collection.find({'characters':[]}).limit(1))
-    try:
-        return ret[0]
-    except IndexError:
-        return []
+    latest = list(collection.find({'characters':[]}).limit(1))
+    if latest == [] and collection.count() > 0:
+        return get_latest()
+    if latest == []:
+        return pages[0]
+    else:
+        i = pages.index(Path(latest[0]['path']))
+        return pages[i] 
 
 def find_one(path):
     return collection.find_one({'path':path})
@@ -61,21 +64,48 @@ def index():
         
         i = pages.index(Path(path))
         if prev:
-            p = pages[i-1]
+            if i - 1 < 0:
+                p = pages[i]
+            else:
+                p = pages[i-1]
         else:
-            p = pages[i+1]
+            try:
+                p = pages[i+1]
+            except IndexError:
+                return 'finished'
     elif request.method == 'GET':
-        latest = get_latest()
-        if latest == []:
-            p = pages[0]
-        else:
-            i = pages.index(Path(latest['path']))
-            p = pages[i+1]
+        p = get_latest()
     
-    prev_data = find_one(p.as_posix())
+    data = find_one(p.as_posix())
     progress = round(collection.count() * 100 / len(pages), 2)
-    return render_template('index.html', path=p, progress=progress, prev_data=prev_data)
+    return render_template('index.html', path=p, progress=progress, data=data)
 
+def skip_next(data, orig):
+    if data is None:
+        return orig
+    if data['useless']:
+        data['characters'] = ['none']
+        collection.update({'path':data['path']}, data, upsert=True)
+        i = pages.index(Path(data['path']))
+        next_data = find_one(pages[i+1].as_posix())
+        if next_data is None:
+            return find_one(pages[i].as_posix())
+        return skip_next(next_data, orig)
+    else:
+        return data
+
+def skip_prev(data, orig):
+    if data['useless']:
+        i = pages.index(Path(data['path']))
+        prev_data = find_one(pages[i-1].as_posix())
+        if prev_data is None:
+            i = pages.index(Path(orig['path']))
+            return find_one(pages[i+1].as_posix())
+        return skip_prev(prev_data, orig)
+    else:
+        return data
+
+# TODO: complete画面, pagesの最後
 @app.route('/tag/', methods=['GET', 'POST'])
 def tag():
     prev = False
@@ -86,7 +116,9 @@ def tag():
             prev = True
         else:
             path = data.get('path')
-            characters = data.get('characters') # TOD
+            characters = list(filter(bool, data.getlist('characters')))
+            if characters == []:
+                characters.append('none')
             data = find_one(path)
             if data:
                 data['characters'] = characters
@@ -96,19 +128,27 @@ def tag():
         
         i = pages.index(Path(path))
         if prev:
-            p = pages[i-1]
+            if i - 1 < 0:
+                p = pages[i]
+            else:
+                p = pages[i-1]
         else:
-            p = pages[i+1]
+            try:
+                p = pages[i+1]
+            except IndexError:
+                return 'finished'
     elif request.method == 'GET':
-        latest = get_tag_latest()
-        if latest == []:
-            p = pages[0]
-        else:
-            i = pages.index(Path(latest['path']))
-            p = pages[i]
-    prev_data = find_one(p.as_posix())
-    progress = round(collection.find({'characters':{'not':[]}}).count() * 100 / len(pages), 2)
-    return render_template('tag.html', path=p, progress=progress, prev_data=prev_data)
+        p = get_tag_latest()
+    
+    data = find_one(p.as_posix())
+    if prev:
+        data = skip_prev(data, data)
+    else:
+        data = skip_next(data, data)
+    p = Path(data['path']) if data else p
+    characters = {c:True for c in data['characters']} if data else []
+    progress = round(collection.find({'characters':{'$ne':[]}}).count() * 100 / len(pages), 2)
+    return render_template('tag.html', path=p, progress=progress, characters=characters)
 
 
 @app.route('/data/<path:filename>')
